28b4fda8348980ae4a1305de86196f094147fd25
Taylor Vercillo <taylornvcasey@gmail.com>
2022-11-26 17:20:25 -0700

    api working'

--- app/controllers/gods_controller.rb
+++ app/controllers/gods_controller.rb
@@ -1,2 +1,24 @@
 class GodsController < ApplicationController
+
+    def index
+        gods = God.all
+        render json: gods
+    end
+
+    def show 
+        god = God.find_by(id: params[:id])
+        render json: god
+    end 
+
+    def create
+        god = God.create(god_params)
+        render json: god
+    end
+
+    private 
+
+    def god_params
+        params.permit(:name, :romanname, :power, :image)
+    end
+
 end
